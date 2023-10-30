9129b1b65404f58823dc09257acd97cb817ebb3b
kobaken <kentafly88@gmail.com>
2023-10-30 22:27:44 +0900

    add title

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ const app = new Hono()
 app.get('*', renderer)
 
 app.get('/', (c) => {
-  return c.render(<h1>Hello!</h1>)
+  return c.render(<h1>Hello!</h1>, {title: 'kobaken.co'})
 })
 
 export default app
