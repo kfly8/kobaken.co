5b6a1e2321ff6ffb1609e78f8b22a8b89e20dc62
kobaken <kentafly88@gmail.com>
2023-11-27 00:02:54 +0900

    add mail

--- src/index.tsx
+++ src/index.tsx
@@ -4,46 +4,49 @@ import { renderer } from './renderer'
 const app = new Hono()
 
 const Profile = () => {
-    return (
-      <>
-        <section class="profile">
-            <img class="icon" src="/static/img/kobaken.jpg" alt="icon" />
-            <div>
-              <h1 class="name">kobaken</h1>
-              <p class="tagline">Software Engineer | Tech Conference Organizer</p>
-              <ul class="sns-links">
-                <li><a href="https://kfly8.hatenablog.com/"><i class="icon-rss"></i><span class="sr-only">Blog</span></a></li>
-                <li><a href="https://github.com/kfly8"><i class="icon-github"></i><span class="sr-only">GitHub</span></a></li>
-                <li><a href="https://twitter.com/kfly8"><i class="icon-twitter"></i><span class="sr-only">Twitter</span></a></li>
-              </ul>
-            </div>
-        </section>
-      </>
-    )
+  return (
+    <>
+      <section class="profile">
+          <img class="icon" src="/static/img/kobaken.jpg" alt="icon" />
+          <div>
+            <h1 class="name">kobaken</h1>
+            <p class="tagline">Software Engineer | Tech Conference Organizer</p>
+            <ul class="sns-links">
+              <li><a href="https://kfly8.hatenablog.com/"><i class="icon-rss"></i><span class="sr-only">Blog</span></a></li>
+              <li><a href="https://github.com/kfly8"><i class="icon-github"></i><span class="sr-only">GitHub</span></a></li>
+              <li><a href="https://twitter.com/kfly8"><i class="icon-twitter"></i><span class="sr-only">Twitter</span></a></li>
+              <li><a href="mailto:kentafly88@gmail.com"><i class="icon-mail"></i><span class="sr-only">Mail</span></a></li>
+            </ul>
+          </div>
+      </section>
+    </>
+  )
+}
+
 }
 
 const Activities = () => {
-    const activities = [
-      { url: 'https://kfly8.hatenablog.com/entry/2023/06/17/133950', title: '最近プログラミングが楽しい', date: '2023-06-17' },
-      { url: 'https://kfly8.hatenablog.com/entry/2023/06/17/133950', title: '最近プログラミングが楽しい', date: '2023-06-17' },
-      { url: 'https://kfly8.hatenablog.com/entry/2023/06/17/133950', title: '最近プログラミングが楽しい', date: '2023-06-17' },
-      { url: 'https://kfly8.hatenablog.com/entry/2023/06/17/133950', title: '最近プログラミングが楽しい', date: '2023-06-17' },
-      { url: 'https://kfly8.hatenablog.com/entry/2023/06/17/133950', title: '最近プログラミングが楽しい', date: '2023-06-17' },
-      { url: 'https://kfly8.hatenablog.com/entry/2023/06/17/133950', title: '最近プログラミングが楽しい', date: '2023-06-17' },
-    ]
+  const activities = [
+    { url: 'https://kfly8.hatenablog.com/entry/2023/06/17/133950', title: '最近プログラミングが楽しい', date: '2023-06-17' },
+    { url: 'https://kfly8.hatenablog.com/entry/2023/06/17/133950', title: '最近プログラミングが楽しい', date: '2023-06-17' },
+    { url: 'https://kfly8.hatenablog.com/entry/2023/06/17/133950', title: '最近プログラミングが楽しい', date: '2023-06-17' },
+    { url: 'https://kfly8.hatenablog.com/entry/2023/06/17/133950', title: '最近プログラミングが楽しい', date: '2023-06-17' },
+    { url: 'https://kfly8.hatenablog.com/entry/2023/06/17/133950', title: '最近プログラミングが楽しい', date: '2023-06-17' },
+    { url: 'https://kfly8.hatenablog.com/entry/2023/06/17/133950', title: '最近プログラミングが楽しい', date: '2023-06-17' },
+  ]
 
-    return (
-      <>
-        <section class="activities">
-          <h2>Activities</h2>
-          <ul>
-            { activities.map((activity) => (
-              <li><span class="date">{activity.date}</span> <a href={activity.url}>{activity.title}</a></li>
-            ))}
-          </ul>
-        </section>
-      </>
-    )
+  return (
+    <>
+      <section class="activities">
+        <h2>Activities</h2>
+        <ul>
+          { activities.map((activity) => (
+            <li><span class="date">{activity.date}</span> <a href={activity.url}>{activity.title}</a></li>
+          ))}
+        </ul>
+      </section>
+    </>
+  )
 }
 
 app.get('*', renderer)
--- src/renderer.tsx
+++ src/renderer.tsx
@@ -17,7 +17,7 @@ export const renderer = jsxRenderer(
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <link rel="icon" type="image/jpg" href="/static/img/favicon.ico" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;900&display=swap" rel="stylesheet" />
-          <link href="/static/fontello-embedded.css" rel="stylesheet" />
+          <link href="/static/fontello-embedded.css?v=2023-11-26" rel="stylesheet" />
           <link href="/static/reset.css?v=2023-11-26" rel="stylesheet" />
           <link href="/static/style.css?v=2023-11-26" rel="stylesheet" />
           <title>{title}</title>
