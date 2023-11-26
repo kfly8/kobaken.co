fcbe892b5b0e5a9ca3101365d967bbdf5f245975
kobaken <kentafly88@gmail.com>
2023-11-26 22:13:13 +0900

    add 400 weight font

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -16,7 +16,7 @@ export const renderer = jsxRenderer(
           <meta http-equiv="X-UA-Compatible" content="IE=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <link rel="icon" type="image/jpg" href="/static/img/favicon.ico" />
-          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@900&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;900&display=swap" rel="stylesheet" />
           <link href="/static/fontello-embedded.css" rel="stylesheet" />
           <link href="/static/reset.css?v=2023-11-26" rel="stylesheet" />
           <link href="/static/style.css?v=2023-11-26" rel="stylesheet" />
