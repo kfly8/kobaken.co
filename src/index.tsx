d3d06760d47648498bc4e8c88a1e9d6c381e8572
kobaken <kentafly88@gmail.com>
2023-11-28 00:00:38 +0900

    update activities

--- src/index.tsx
+++ src/index.tsx
@@ -52,19 +52,20 @@ const Activities = () => {
     { date: '2019-02-05', url: 'https://blog.yapcjapan.org/entry/2019/02/05/160000', title: 'YAPC::Tokyoを終えて' },
     { date: '2019-09-09', url: 'https://tech.mobilefactory.jp/entry/2019/09/09/180000', title: 'The Perl Conference 2019 in Pittsburghに行ってきた' },
     { date: '2020-07-21', url: 'https://speakerdeck.com/kfly8/ming-ri-karadekiruxin-ren-falseonbodeingugaido',  title: '明日からできる新人のオンボーディングガイド | #kichijojipm 23' },
-    { date: '2020-08-30', url: 'https://github.com/isucon/isucon10-qualify/pull/117', title: 'isucon10でPerlの参考実装移植をしました' },
+    { date: '2020-08-30', url: 'https://github.com/isucon/isucon10-qualify/pull/117', title: 'ISUCON10で参考実装のPerl移植をしました' },
     { date: '2021-03-30', url: 'https://speakerdeck.com/kfly8/yan-xiu-haibentodenakukomiyuniteizuo-ri', title: '研修はイベントでなくコミュニティ作り | #kichijojipm 26' },
-    { date: '2021-08-13', url: 'https://github.com/isucon/isucon11-qualify/pull/1099', title: 'isucon11でPerlの参考実装移植をしました' },
+    { date: '2021-08-13', url: 'https://github.com/isucon/isucon11-qualify/pull/1099', title: 'ISUCON11で参考実装のPerl移植をしました' },
     { date: '2021-12-15', url: 'https://kfly8.hatenablog.com/entry/2021/12/15/213610', title: 'WEB+DB Press Vol.126で「ISUCONの実装から最近のPerlを学ぶ」という題の記事を寄稿しました' },
     { date: '2021-12-25', url: 'https://tech.mobilefactory.jp/entry/2021/12/25/000000', title: '技術ブログが書ける開発をする'},
     { date: '2022-06-30', url: 'https://tech.mobilefactory.jp/entry/2022/06/30/200000', title: 'Perlの技術研修' },
-    { date: '2022-07-14', url: 'https://github.com/isucon/isucon12-qualify/pull/164', title: 'isucon12でPerlの参考実装移植をしました' },
+    { date: '2022-07-14', url: 'https://github.com/isucon/isucon12-qualify/pull/164', title: 'ISUCON12で参考実装のPerl移植をしました' },
     { date: '2022-07-22', url: 'https://tech.mobilefactory.jp/entry/2022/07/22/184000', title: 'OSSのスポンサーを始めるときに、技術広報目線で考えたこと' },
     { date: '2022-08-30', url: 'https://kfly8.hatenablog.com/entry/2022/08/30/185300', title: 'エンジニアのためのお祭りの技術カンファレンスをオンラインで実施する｜YAPC::Japan::Onlineの場合' },
     { date: '2023-03-22', url: 'https://kfly8.hatenablog.com/entry/2023/03/22/015343', title: 'YAPC::Kyoto 2023 の運営をしました' },
     { date: '2023-06-17', url: 'https://kfly8.hatenablog.com/entry/2023/06/17/133950', title: '最近プログラミングが楽しい' },
     { date: '2023-08-26', url: 'https://kfly8.hatenablog.com/entry/2023/08/26/214805', title: 'ドキュメントでプログラミング言語に貢献する' },
     { date: '2023-11-09', url: 'https://kfly8.hatenablog.com/entry/2023/11/09/092623', title: 'perldoc.jp のこの1年の変化まとめと、次の1年' },
+    { date: '2023-11-25', url: 'https://github.com/isucon/isucon13', title: 'ISUCON13で参考実装のPerl移植をしました' },
   ]
 
   const formatMMDD = (date: Date) => {
