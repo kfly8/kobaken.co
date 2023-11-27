c54a64b7cd6e5a2416b02e329cc2ce995da5f44f
kobaken <kentafly88@gmail.com>
2023-11-27 20:33:05 +0900

    活動まわりを調整

--- src/index.tsx
+++ src/index.tsx
@@ -43,10 +43,13 @@ const Activities = () => {
     { date: '2019-02-05', url: 'https://blog.yapcjapan.org/entry/2019/02/05/160000', title: 'YAPC::Tokyoを終えて' },
     { date: '2019-09-09', url: 'https://tech.mobilefactory.jp/entry/2019/09/09/180000', title: 'The Perl Conference 2019 in Pittsburghに行ってきた' },
     { date: '2020-07-21', url: 'https://speakerdeck.com/kfly8/ming-ri-karadekiruxin-ren-falseonbodeingugaido',  title: '明日からできる新人のオンボーディングガイド | #kichijojipm 23' },
+    { date: '2020-08-30', url: 'https://github.com/isucon/isucon10-qualify/pull/117', title: 'isucon10でPerlの参考実装移植をしました' },
     { date: '2021-03-30', url: 'https://speakerdeck.com/kfly8/yan-xiu-haibentodenakukomiyuniteizuo-ri', title: '研修はイベントでなくコミュニティ作り | #kichijojipm 26' },
+    { date: '2021-08-13', url: 'https://github.com/isucon/isucon11-qualify/pull/1099', title: 'isucon11でPerlの参考実装移植をしました' },
     { date: '2021-12-15', url: 'https://kfly8.hatenablog.com/entry/2021/12/15/213610', title: 'WEB+DB Press Vol.126で「ISUCONの実装から最近のPerlを学ぶ」という題の記事を寄稿しました' },
     { date: '2021-12-25', url: 'https://tech.mobilefactory.jp/entry/2021/12/25/000000', title: '技術ブログが書ける開発をする'},
     { date: '2022-06-30', url: 'https://tech.mobilefactory.jp/entry/2022/06/30/200000', title: 'Perlの技術研修' },
+    { date: '2022-07-14', url: 'https://github.com/isucon/isucon12-qualify/pull/164', title: 'isucon12でPerlの参考実装移植をしました' },
     { date: '2022-07-22', url: 'https://tech.mobilefactory.jp/entry/2022/07/22/184000', title: 'OSSのスポンサーを始めるときに、技術広報目線で考えたこと' },
     { date: '2022-08-30', url: 'https://kfly8.hatenablog.com/entry/2022/08/30/185300', title: 'エンジニアのためのお祭りの技術カンファレンスをオンラインで実施する｜YAPC::Japan::Onlineの場合' },
     { date: '2023-03-22', url: 'https://kfly8.hatenablog.com/entry/2023/03/22/015343', title: 'YAPC::Kyoto 2023 の運営をしました' },
@@ -55,14 +58,28 @@ const Activities = () => {
     { date: '2023-11-09', url: 'https://kfly8.hatenablog.com/entry/2023/11/09/092623', title: 'perldoc.jp のこの1年の変化まとめと、次の1年' },
   ]
 
+  const formatMMDD = (date: Date) => {
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = (date.getDate()).toString().padStart(2, '0');
+    return `${month}-${day}`
+  }
+
+  const displayActivities = activities.reverse().map((activity, index, arr) => {
+    const currentDate  = new Date(activity.date);
+    const previousDate = index > 0 ? new Date(arr[index - 1].date) : null;
+    const displayDate  = previousDate && currentDate.getFullYear() === previousDate.getFullYear() ? formatMMDD(currentDate) : activity.date
+
+    return { ...activity, displayDate }
+  });
+
   return (
     <>
       <section id="activities">
         <h2>Activities</h2>
         <ul>
-          { activities.reverse().map((activity) => (
+          { displayActivities.map((activity) => (
             <li>
-              <span class="date">{activity.date}</span>
+              <span class="date">{activity.displayDate}</span>
               <span class="title"><a href={activity.url}>{activity.title}</a></span>
             </li>
           ))}
