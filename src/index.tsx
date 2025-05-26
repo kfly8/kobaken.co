f75eab1833fca769cfac0835a9e9cca0a4a80c21
kobaken <kentafly88@gmail.com>
2025-05-26 18:37:56 +0900

    Added test api

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 import { renderer } from './renderer'
 
 import { Root } from './components/root'
 
 const app = new Hono()
 
-app.get('*', renderer)
+app.use('/api/*', cors())
+app.use('*', renderer)
 
 app.get('/', (c) => {
-
   return c.render(
     <Root />,
     {
@@ -16,6 +17,12 @@ app.get('/', (c) => {
         description: "I'm a software engineer and preparing to start my own business. Event organizer for YAPC::Tokyo 2019, YAPC::Japan::Online 2022, and YAPC::Hiroshima 2024."
     }
   )
+})
+
+app.get('/api/hello', (c) => {
+  return c.json({
+    message: 'Hello, World!'
+  })
 })
 
 export default app
